f6787f240012c2659161d7534e243134caac7745
hardishc <contacthardish@gmail.com>
2022-08-23 11:26:53 -0600

    Create darkmode

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 import MapLeaflet from './components/MapLeaflet'
+import {Button, Container, Grid, Paper, styled} from "@mui/material";
 
 function App() {
+    const Item = styled(Paper)(({ theme }) => ({
+        backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+        ...theme.typography.body2,
+        padding: theme.spacing(1),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    }));
+    return (
+        <Grid container spacing={2}>
+            <Item> <MapLeaflet/></Item>
+        </Grid>
 
-  return (
-        <MapLeaflet/>
-  );
+    );
 }
 
 export default App;
--- src/components/MapLeaflet.js
+++ src/components/MapLeaflet.js
@@ -1,44 +1,73 @@
-import {Circle, MapContainer, Marker, Popup, TileLayer} from 'react-leaflet'
+import {Circle, MapContainer, Marker, Popup, TileLayer, LayersControl, Tooltip} from 'react-leaflet'
 import {useEffect, useState} from "react";
 import axios from "axios";
+import customMarker from './assets/tree-color-icon.svg';
+import L from 'leaflet';
+import {Button} from "@mui/material";
+const {MainLayer} = LayersControl
+
+const iconPerson = new L.Icon({
+    iconUrl: customMarker,
+    iconRetinaUrl: customMarker,
+    popupAnchor:  [-0, -0],
+    iconSize: [20],
+});
 
 function MapLeaflet () {
     const defaultCenter = [51.041394, -114.063579];
     const [trees, setTrees] = useState();
+    const [day, setDay] = useState(true)
+
+    const NightMap = () => (
+        <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url=" 	https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png"
+        />
+    )
+
+    const DayMap = () => (
+        <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
+        />
+    )
 
     useEffect(() => {
-        axios.get(`https://data.calgary.ca/resource/tfs4-3wwa.json`)
+        axios.get(`https://data.calgary.ca/resource/tfs4-3wwa.json?$where=heritage_trees='Y'&$limit=50000`)
             .then(res => {
-                const data = res.data;
+                const data = res.data
                 setTrees(data);
             })
     }, []);
+
     return (
-
-        <MapContainer center={defaultCenter} zoom={13} scrollWheelZoom={true}>
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            {trees &&
-                <>
-                    {trees.map((marker) => {
-                        const markerPos = marker.point.coordinates
-                        return (
-                            <Marker position={
-                                [
-                                    markerPos[1],
-                                    markerPos[0]
-                                ]} key={marker.wam_id}>
-                                <Popup>
-                                    A pretty CSS3 popup. <br /> Easily customizable.
-                                </Popup>
-                            </Marker>
-                        )})}
-                </>
-            }
-        </MapContainer>
-
+        <>
+            <Button onClick={()=>setDay(!day)}>Change mode</Button>
+            <MapContainer center={defaultCenter} zoom={13} scrollWheelZoom={true}>
+                {day ?
+                    <DayMap/>
+                    :
+                    <NightMap/>
+                }
+                {trees &&
+                    <>
+                        {trees.map((marker) => {
+                            const lat = marker.point.coordinates[1]
+                            const lng = marker.point.coordinates[0]
+                            return (
+                                <Marker position={[lat,lng]} key={marker.wam_id} icon={iconPerson} autoPanOnFocus={false}>
+                                    <Tooltip>
+                                        {marker.common_name}
+                                    </Tooltip>
+                                    <Popup>
+                                        {marker.common_name}
+                                    </Popup>
+                                </Marker>
+                            )})}
+                    </>
+                }
+            </MapContainer>
+        </>
     )
 }
 
